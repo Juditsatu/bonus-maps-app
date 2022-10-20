564593878a98d68e08b54b1c848b3f7cb8e4a6ed
Judisatu <judit.sant@gmail.com>
2022-10-20 19:38:38 +0200

    fly to map location

--- src/app/maps/components/btn-my-location/btn-my-location.component.ts
+++ src/app/maps/components/btn-my-location/btn-my-location.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { MapService, PlacesService } from '../../services';
 
 @Component({
   selector: 'app-btn-my-location',
@@ -7,9 +8,14 @@ import { Component } from '@angular/core';
 })
 export class BtnMyLocationComponent{
 
-  constructor() { }
+  constructor( private placesService: PlacesService, private mapService: MapService ) { }
 
   goToMyLocation() {
+
+    if (!this.placesService.isUserLocationReady) throw Error("There's no user location");
+    if (!this.mapService.isMapReady) throw Error("No map available");
+
+    this.mapService.flyTo( this.placesService.userLocation! )
 
   }
 
