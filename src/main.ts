0a54d3898c30cb287d4343d1e919b92d1ea20534
Judisatu <judit.sant@gmail.com>
2022-10-20 19:30:44 +0200

    added mapbox

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,10 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+import Mapboxgl from 'mapbox-gl'; // or "const mapboxgl = require('mapbox-gl');"
+ 
+Mapboxgl.accessToken = 'pk.eyJ1IjoianVkaXRzYXR1IiwiYSI6ImNsN2VuNTF3OTAyMGYzbmxxMzZlaTd4Y3YifQ.RhukOPcTrVMr2JtoIlRdzA';
+
 if (!navigator.geolocation) {
   alert("This browser doesn't support Geolocation");
   throw new Error("This browser doesn't support Geolocation");
