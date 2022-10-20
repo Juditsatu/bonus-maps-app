8c3c5182280d4660bb30b61c5916d3624f93ebb2
Judisatu <judit.sant@gmail.com>
2022-10-20 19:30:33 +0200

    added components

--- src/app/maps/components/map-view/map-view.component.ts
+++ src/app/maps/components/map-view/map-view.component.ts
@@ -1,17 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
 import { PlacesService } from '../../services';
+import { Map, Popup, Marker } from 'mapbox-gl';
 
 @Component({
   selector: 'app-map-view',
   templateUrl: './map-view.component.html',
   styleUrls: ['./map-view.component.css']
 })
-export class MapViewComponent implements OnInit {
+export class MapViewComponent implements AfterViewInit {
+
+  @ViewChild('mapDiv')
+  mapDivElement!: ElementRef
 
   constructor( private placesService: PlacesService ) { }
 
-  ngOnInit(): void {
-    console.log(this.placesService.userLocation)
+  ngAfterViewInit(): void {
+
+    if (!this.placesService.userLocation) throw Error('No hay placesService.userLocation')
+
+    const map = new Map({
+      container: this.mapDivElement.nativeElement,
+      style: 'mapbox://styles/mapbox/light-v10', // style URL
+      center: this.placesService.userLocation,
+      zoom: 14,
+    });
+
+    const popup = new Popup()
+      .setHTML(`
+        <h6> I'm here </h6>
+        <span> I'm here in this part of the world </span>
+      `);
+
+    new Marker({ color: 'red' })
+      .setLngLat( this.placesService.userLocation )
+      .setPopup( popup )
+      .addTo( map )
+      
   }
 
 }
