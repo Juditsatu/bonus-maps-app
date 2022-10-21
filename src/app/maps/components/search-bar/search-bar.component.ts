21e9ed89e187069a06154efea0dc23a8e265fa22
Judisatu <judit.sant@gmail.com>
2022-10-21 19:58:00 +0200

    search components

---
+++ src/app/maps/components/search-bar/search-bar.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-search-bar',
+  templateUrl: './search-bar.component.html',
+  styleUrls: ['./search-bar.component.css']
+})
+export class SearchBarComponent {
+
+  private debounceTimer?: NodeJS.Timeout;
+
+  constructor() { }
+
+
+  onQueryChanged(query: string = '') {
+    
+    if (this.debounceTimer) clearTimeout(this.debounceTimer);
+
+    this.debounceTimer = setTimeout(() => {
+        console.log('Enviar query', query)
+    }, 500);
+  }
+
+}
